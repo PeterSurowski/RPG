52976fa7193d2dedf61e71184ed65756d3cbb265
Peter Surowski <Peter Surowski>
2017-07-08 15:30:56 -0700

    Fight screen appears.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,14 +1,17 @@
 function instructionsHider() {
 	var instructionsHider = document.getElementById('instructions');
-	instructionsHider.style.display = "none";
-	var firstAppears = document.getElementById('first-zombie');
-	firstAppears.className = 'unhidden';
-	var secondAppears = document.getElementById('second-zombie');
-	secondAppears.className = 'unhidden';
-	var thirdAppears = document.getElementById('third-zombie');
-	thirdAppears.className = 'unhidden';
-	var chooseAppears = document.getElementById('choose');
-	chooseAppears.className = 'unhidden';
+	instructionsHider.className = 'hidden';
+	setTimeout(function() {
+		instructionsHider.style.display = "none";
+		var firstAppears = document.getElementById('first-zombie');
+		firstAppears.className = 'unhidden';
+		var secondAppears = document.getElementById('second-zombie');
+		secondAppears.className = 'unhidden';
+		var thirdAppears = document.getElementById('third-zombie');
+		thirdAppears.className = 'unhidden';
+		var chooseAppears = document.getElementById('choose');
+		chooseAppears.className = 'unhidden';	
+	}, 2000);	
 }
 
 function fightZombie() {
@@ -16,5 +19,8 @@ function fightZombie() {
 	backgroundChanger.className = 'background-fade background-mover';
 	setTimeout(function() {
 		backgroundChanger.style.display = 'none';
+		var fightScreen = document.getElementById('fight-screen');
+		fightScreen.style.display = 'block';
+		fightScreen.className = 'unhidden';
 	}, 2000);
 }
