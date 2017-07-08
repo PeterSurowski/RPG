f8cedff9bc88556afadfc45d1c901d14dd8f8dc7
Peter Surowski <Peter Surowski>
2017-07-08 10:26:06 -0700

    Made background element sink and fade.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,20 @@
 function instructionsHider() {
 	var instructionsHider = document.getElementById('instructions');
 	instructionsHider.style.display = "none";
-	var zombiesAppear = document.getElementById('first-zombie');
-	zombiesAppear.className = 'unhidden';
+	var firstAppears = document.getElementById('first-zombie');
+	firstAppears.className = 'unhidden';
+	var secondAppears = document.getElementById('second-zombie');
+	secondAppears.className = 'unhidden';
+	var thirdAppears = document.getElementById('third-zombie');
+	thirdAppears.className = 'unhidden';
 	var chooseAppears = document.getElementById('choose');
 	chooseAppears.className = 'unhidden';
+}
+
+function fightZombie() {
+	var backgroundChanger = document.getElementById('background');
+	backgroundChanger.className = 'background-fade background-mover';
+	setTimeout(function() {
+		backgroundChanger.style.display = 'none';
+	}, 2000);
 }
