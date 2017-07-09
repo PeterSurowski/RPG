5d719a8c80fac0431fa1d3f8df47585774a81cb6
Peter Surowski <Peter Surowski>
2017-07-09 00:13:15 -0700

    All zombies disappearing after HP reaches 0, but still needs work.

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,7 @@ var opponentOneHp = 30;
 var opponentTwoHp = 50;
 var opponentThreeHp = 20;
 var playerHp = 100;
+var ranNumPlayer, ranNumZombieOne, ranNumZombieTwo,ranNumZombieThree;
 
 function instructionsHider() {
 	var instructionsHider = document.getElementById('instructions');
@@ -22,24 +23,24 @@ function instructionsHider() {
 
 function zombieImgAdderOne() {
 	document.getElementById('opponent-pic').innerHTML = '<img src="assets/images/zombie-one-cropped-243X355.png">';
-	document.getElementById('opponent-info').innerHTML = '<p id="description"><b>Description:</b> He looks as though he were once a strong man, but he has been dead for a long time and decay has somewhat weakened him. Take him down, but be careful.</p><p id="difficulty"><b>Difficulty:</b> Medium</p>';
-	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP: </b>' + opponentOneHp + '</h3>';
-	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP: </b>' + playerHp + '</h3>';
+	document.getElementById('opponent-info').innerHTML = '<p id="description"><b>Description:</b> He was probably once a strong man, but he died long ago and decay has somewhat weakened him. Take him down, but be careful.</p><p id="difficulty"><b>Difficulty:</b> Medium</p>';
+	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP :  </b>' + opponentOneHp + '</h3>';
+	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP :  </b>' + playerHp + '</h3>';
 }
 
 function zombieImgAdderTwo() {
 	document.getElementById('opponent-pic').innerHTML = '<img src="assets/images/zombie-two-355X464.png">';
 	document.getElementById('opponent-info').innerHTML = '<p id="description"><b>Description:</b> Somehow his dead eyes seem to seeth with anger. He is strong and fast, but the strength of his attacks is erratic.</p><p id="difficulty"><b>Difficulty:</b> Hard</p>';
-	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP: </b>' + opponentTwoHp + '</h3>';
-	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP: </b>' + playerHp + '</h3>';
+	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP :  </b>' + opponentTwoHp + '</h3>';
+	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP :  </b>' + playerHp + '</h3>';
 }
 
 
 function zombieImgAdderThree() {
 	document.getElementById('opponent-pic').innerHTML = '<img src="assets/images/zombie-three-214X355.png">';
 	document.getElementById('opponent-info').innerHTML = '<p id="description"><b>Description:</b> He has not been dead for long, but he is missing an arm. Dispatch him quickly.</p><p id="difficulty"><b>Difficulty:</b> Easy</p>';
-	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP: </b>' + opponentThreeHp + '</h3>';
-	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP: </b>' + playerHp + '</h3>';
+	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP :  </b>' + opponentThreeHp + '</h3>';
+	document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP :  </b>' + playerHp + '</h3>';
 }
 
 
@@ -55,5 +56,37 @@ function fightZombie() {
 }
 
 function fightButton() {
-	alert('fight button working');
+	//Puts a flash of red behind opponent-pic.
+	document.getElementById('opponent-pic').classList.add('overlay');
+	setTimeout(function() {
+		document.getElementById('opponent-pic').classList.remove('overlay');
+	}, 50);
+	//Takes some HP from opponent.
+	ranNumPlayer = Math.floor((Math.random() * 10) + 10);
+	opponentOneHp = opponentOneHp - ranNumPlayer;
+	document.getElementById('opponent-hp').innerHTML = '<h3 id="hp"><b>Enemy HP :  </b>' + opponentOneHp + '</h3>';
+	//If opponent HP reaches 0, return to "choose" screen.
+	if (opponentOneHp <= 0) {
+		alert('opponent hp is 0 or lower.')
+		var fightScreen = document.getElementById('fight-screen');
+		fightScreen.className = 'background-fade background-mover';
+		setTimeout(function() {
+			fightScreen.style.display = 'none';
+			var backgroundUnhider = document.getElementById('background');
+			backgroundUnhider.style.display = 'block';
+			backgroundUnhider.className = 'unhidden';
+		}, 2000);
+	}
+	//Waits a moment, then takes some HP from player.
+	ranNumZombieOne = Math.floor((Math.random() * 10) + 10);
+	setTimeout(function() {
+		playerHp = playerHp - ranNumZombieOne;
+		document.getElementById('player-hp').innerHTML = '<h3 id="playerhp"><b>Your HP :  </b>' + playerHp + '</h3>';
+		document.getElementById('fight-screen').classList.add('overlay');
+		//Makes a flash of red on the fight screen.
+		setTimeout(function() {
+			document.getElementById('fight-screen').classList.remove('overlay');
+	}, 50);
+	},1000);
+	
 }
